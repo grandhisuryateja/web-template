9446fa6ffeabfa5c8451670d5edd6ea8cdec5ef9
Grandhi Suryateja <grandhi.suryateja@hashedin.com>
2016-05-31 20:26:50 +0530

    select is working for first div

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,21 +3,12 @@ $(document).ready(function() {
     console.log("hello");
     var total = 0;
     var jsonUrl = null;
+    var flag = 0;
     $('.a').on('click', function(event) {
-    	var self = this;
         // $('#total').html('$'+total);
         ID = $(this).attr('id');
-        // console.log(ID);
-       
-        console.log($(this).children('.imagecontainer').prop('id'));
-        // console.log($(this).children(".imagecontainer").children('.imgclass').prop('src'));
-
-        // $(this).children(".imagecontainer").children('.imgclass').css("content", 'url(' + jsonUrl + ')');
-        // $(this).toggle(function () {
-        // 	// body...
-        // 	$(this).children(".imagecontainer").children('.imgclass').css("content", "url(/img/Assets/Landing%20Page/Step-1/iOS_white.png)");
-        // });
         $(this).toggleClass("select-1");
+        var self = this;
         $.getJSON('price.json', function(result) {
             var Data = result.prices;
             console.log("qwertyuio");
@@ -26,13 +17,26 @@ $(document).ready(function() {
                 if (Data[i].id === ID) {
                     // console.log(Data[i].value, "------");
                     total = total + parseInt(Data[i].value);
-                    jsonUrl = Data[i].url;
+                    whiteImage = Data[i].imageWhite;
+                    image = Data[i].image;
                 }
             }
-            $(self).children(".imagecontainer").children('.imgclass').css("content", 'url(' + jsonUrl + ')').toggle();
+            console.log(self,"self");
+            console.log($(self).hasClass("select-1"),"true or false");
+            if($(self).hasClass("select-1")){
+            	$(self).children(".imagecontainer").children('.imgclass').css("content", 'url(' + whiteImage + ')');	
+            }
+             else{
+             $(self).children(".imagecontainer").children('.imgclass').css("content", 'url(' + image + ')');
+             }   
         });
         console.log("jsonUrl : ",jsonUrl);
         // $(self).children(".imagecontainer").children('.imgclass').css("content", 'url(' + jsonUrl + ')');
+
+    });
+
+    $('.b').on('click', function(event) {
+
 
     });
     $('#total1').html('$' + total);
