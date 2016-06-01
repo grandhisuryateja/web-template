70a2d3f91db6ba84f18f367d844e20e0dfdaf30c
Grandhi Suryateja <grandhi.suryateja@hashedin.com>
2016-06-01 19:04:51 +0530

    selecting one box completed

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,73 +2,76 @@ $(document).ready(function() {
     // body...
     // console.log("hello");
     var total = 0;
+    var Data ;
+    $.getJSON('price.json', function(result) {
+        	// console.log($(self),"self123");
+            Data = result.prices;
+   	       // console.log("qwertyuio");
+             });
+
     $('.a').on('click', function(event) {
         // $('#total').html('$'+total);
         ID = $(this).attr('id');
         $(this).toggleClass("select-1");
         var self = this;
-        $.getJSON('price.json', function(result) {
-            var Data = result.prices;
             // console.log("qwertyuio");
             for (var i in Data) {
                 if (Data[i].id === ID) {
-                    // console.log(Data[i].value, "------");
-                    total = total + parseInt(Data[i].value);
+                    cost = Data[i].value;
                     whiteImage = Data[i].imageWhite;
                     image = Data[i].image;
                 }
             }
             if($(self).hasClass("select-1")){
             	// console.log($(self).children(".imagecontainer").children('.imgclass').attr("src"));
+            	total = total + parseInt(cost);
             	$(self).children(".imagecontainer").children('.imgclass').attr("src", whiteImage);	
             }
              else{
-             $(self).children(".imagecontainer").children('.imgclass').attr("src", image);;
-             }   
-        });
+             	total = total - parseInt(cost);
+                $(self).children(".imagecontainer").children('.imgclass').attr("src", image);;
+             } 
+             console.log(total,"total-a");  
     });
 
+    
+
     $('.b').on('click', function(event) {
-    	console.log("bbbbbbbbbbbbbbbb");
     	//ID = $(this).attr('id');
-        $('.b').removeClass("select-1");
+        var mainId = $(this).parent().attr('id');
+        $('#'+mainId).find('.b').removeClass("select-1");
+        // console.log($('#'+mainId).find('.b'),"$('#'+mainId).find('.b')");
         $(this).addClass("select-1");	
-         var self = this;
-         var mainId = $(this).parent().attr('id')
-         console.log(mainId,"parentid");
-         count = $('#three .b').length;
-         for(i = 0 ; i < count ; i++)
+         // console.log(mainId,"parentid");
+         count = $('#'+mainId).find('.b').length;
+         // console.log(count,"count");
+         // console.log(Data,"Data");
+         // var price_info = Call_my
+        for(j = 0 ; j < count ; j++)
          {
 		
-			ID = $('#three .b').eq(i).attr('id');
-			self = $('#three .b').eq(i);
-			// console.log(self,"self[i]");        	
-    //       $('#'+mainId).each(function () {
-    //       	console.log("insideeach");	
-    //         console.log($(this).attr('id'),"onetwothree");
-    // });	
-        $.getJSON('price.json', function(result) {
-            var Data = result.prices;
-            // console.log("qwertyuio");
-            for (var i in Data) {
+			ID = $('#'+mainId).find('.b').eq(j).attr('id');
+			self = $('#'+mainId).find('.b').eq(j);
+			// console.log($(self),"self123");
+			for (var i in Data) {
                 if (Data[i].id === ID) {
-                    // console.log(Data[i].value, "------");
-                    total = total + parseInt(Data[i].value);
+                    cost = Data[i].value;
                     whiteImage = Data[i].imageWhite;
                     image = Data[i].image;
                 }
             }
             if($(self).hasClass("select-1")){
-            	console.log("111111111111");
+            	total = total + parseInt(cost);
             	$(self).children(".imagecontainer").children('.imgclass').attr("src", whiteImage);	
             }
              else{
-             	console.log("2222222222222222");
+             	total = total - parseInt(cost);	
              $(self).children(".imagecontainer").children('.imgclass').attr("src", image);;
              }   
-        });
-    }
+    }  
+    console.log(total,"total-b");
     });
-    $('#total1').html('$' + total);
-    console.log("total :", total);
+
+    // $('#total1').html('$' + total);
+    // console.log("total :", total);
 });
