561f964147345d17d60beb1f32de58431a4c064e
Grandhi Suryateja <grandhi.suryateja@hashedin.com>
2016-06-09 01:21:38 +0530

    Price functionality added.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
     // body...
     var total = 0;
-    var Data ;
+    var jsonData ;
     var dict = [];
     var data;
+    var selectedValue = 0;
+    var selected = 0;
+
+    /**
+    *
+    Getting the whole data from json.*/
     $.getJSON('price.json', function(result) {
-            Data = result.prices;
-
+            jsonData = result.prices;
              });
-
-     function dictionary(Data)
+    /**
+    *
+    Converting the json to dictionary with key as id.*/
+     function dictionary(jsonData)
     {
-    	data = Data;
+    	data = jsonData;
     	for(var i = 0; i < data.length ; i++)
     	{
     		temp = [];
@@ -23,7 +30,7 @@ $(document).ready(function() {
     	return dict;
     }
 
-   var $steps = $('.step');
+  var $steps = $('.step');
   var currentStep = 0,
       nextStep;
   
@@ -54,14 +61,80 @@ var w= $(window).width();
 var ww=parseInt($('.background').css('width'));
 var marginLeft=w/2-ww/2;
 $('.background').css('margin-left',marginLeft + "px");
-$('#bullet13').css('color','#fa6023');
 console.log(ww);
-console.log(text,"text");
+// console.log(text,"text");
 
 $('.bullet').click(function(){
 	var myid=parseInt($(this).attr('id').replace(/[a-z]/g, ''))
 	console.log("This is id "+ myid);
 	$("step-"+myid).css('margin-left','0px');
 });
+
+ $('.multiple-click').on('click', function(event) {
+       dict =  dictionary(jsonData);
+        id = $(this).attr('id');
+        // console.log(dict[ID]['imageWhite'],"dictionary");
+        $(this).toggleClass("click-selected");
+        var self = this;
+        cost = dict[id]['value'];
+        whiteImage = dict[id]['imageWhite'];
+        image = dict[id]['image'];
+        if($(self).hasClass("click-selected")){
+        	total = total + parseInt(cost);
+        	$(self).children(".imagecontainer").children('.imgclass').attr("src", whiteImage);	
+        }
+         else{
+         	total = total - parseInt(cost);
+            $(self).children(".imagecontainer").children('.imgclass').attr("src", image);;
+         }
+        console.log(total,"total");     	
+    });
+
+ $('.single-click').on('click', function(event) {
+        var mainId = $(this).parent().parent().attr('id');
+        console.log(mainId,"mainId");
+        dict =  dictionary(jsonData);
+        count = $('#'+mainId).find('.single-click').length;
+         for(k = 0 ; k < count ; k++)
+         {
+			id = $('#'+mainId).find('.single-click').eq(k).attr('id');
+			self = $('#'+mainId).find('.single-click').eq(k);
+   			cost = dict[id]['value'];
+   			console.log(cost,'cost');
+            if($(self).hasClass("click-selected"))
+            {
+            	selectedValue = cost;
+            	selected = 1;
+            }   
+    }
+        $('#'+mainId).find('.single-click').removeClass("click-selected");
+         // console.log($('#'+mlainId).find('.b'),"$('#'+mainId).find('.b')");
+        $(this).addClass("click-selected");	
+         // console.log(mainId,"parentid");
+        for(j = 0 ; j < count ; j++)
+         {
+			id = $('#'+mainId).find('.single-click').eq(j).attr('id');
+			self = $('#'+mainId).find('.single-click').eq(j);
+   			cost = dict[id]['value'];
+        	whiteImage = dict[id]['imageWhite'];
+        	image = dict[id]['image'];
+            if($(self).hasClass("click-selected")){
+            	total = total + parseInt(cost);
+            	$(self).children(".imagecontainer").children('.imgclass').attr("src", whiteImage);	
+            }
+             else{
+
+             $(self).children(".imagecontainer").children('.imgclass').attr("src", image);;
+             }   
+    }
+             if(selected === 1){
+             	total = total-selectedValue;
+             } 
+             selected = 0; 
+              console.log(total,selectedValue,"total,temp");
+    });
+
+//console.log(total,"total");
+
 });
 
