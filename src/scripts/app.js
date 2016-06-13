2e9d8df58bd4c616441c8610532e4e5ac61cc978
Grandhi Suryateja <grandhi.suryateja@hashedin.com>
2016-06-13 22:49:07 +0530

    Progress bar functionality is working.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,112 +1,111 @@
 $(document).ready(function() {
-    // body...
-    var total = 0;
-    var jsonData ;
-    var dict = [];
-    var data;
-    var selectedValue = 0;
-    var selected = 0;
 
+    var total = 0,
+        jsonData, data, selectedValue = 0,
+        selected = 0,dict = [];
     /**
     *
     Getting the whole data from json.*/
     $.getJSON('price.json', function(result) {
-            jsonData = result.prices;
-             });
+        jsonData = result.prices;
+    });
     /**
     *
     Converting the json to dictionary with key as id.*/
-     function dictionary(jsonData)
-    {
-      data = jsonData;
-      for(var i = 0; i < data.length ; i++)
-      {
-        temp = [];
-        temp['value'] = data[i].value;
-        temp['imageWhite'] = data[i].imageWhite;
-        temp['image'] = data[i].image;
-        dict[data[i].id] = temp;
-      }
-      return dict;
+    function dictionary(jsonData) {
+        data = jsonData;
+        for (var i = 0; i < data.length; i++) {
+            temp = [];
+            temp['value'] = data[i].value;
+            temp['imageWhite'] = data[i].imageWhite;
+            temp['image'] = data[i].image;
+            dict[data[i].id] = temp;
+        }
+        return dict;
     }
+    var content = ['Index Page', 'Platform &emsp;', 'Size &emsp;&emsp;', 'User&emsp; Management', 'Data Type', 'Existing Data Sources',
+        'Location Data', 'Third-Party Cloud API', 'User Engagement', 'Quality &emsp;', 'Security &emsp;', 'Analytics'
+    ];
+    // var content = ['Data Data','Data Data','Data Data','Data cata','Data &emsp;','Data Data','Data ata'
+    // ,'Data Data','Data Data','Data Data','Data Data','Data Data','Data Data','Data Data','Data Data','Data Data'];
 
-  var $steps = $('.step');
-  var currentStep = 0,z=0,
-      nextStep;
-  
-  $steps.slice(1).hide(); //hide all but first
+    var $steps = $('.step');
+    var currentStep = 1,
+        prevStep,
+        nextStep;
 
-  $('#singlebutton , #next').on('click', function(e) {
-    e.preventDefault();
-    var stepId = parseInt($(this).parent().parent().parent().attr('id').replace(/[a-z,-]/g, ''));
-    stepId = stepId + 1;
-    console.log($('#bullet'+stepId).attr('class'),'classname');
-    $('#bullet'+stepId).css('color','#fa6023');
-    nextStep = currentStep + 1;
-    console.log(stepId,"stepId");
-    if (nextStep == $steps.length) {
-      alert("You reached the end");
-      return;
-    }
-    $($steps.get(currentStep)).hide();
-    $($steps.get(nextStep)).show();
-    currentStep = nextStep;
-  });
+    $steps.slice(1).hide(); //hide all but first
+    $('#singlebutton').on('click', function() {
+        $($steps.get(0)).hide();
+        $($steps.get(1)).show();
+        $('#diamond-1').addClass('diamond-select');
+        $('.label-1').css('opacity', '1');
+
+    });
+
+    $('[id^=next]').on('click', function(e) {
+        e.preventDefault();
+        var stepId = parseInt($(this).parent().parent().parent().parent().attr('id').replace(/[a-z,-]/g, ''));
+        if(!($('#step-' + stepId ).children().children().children().hasClass('click-selected')))
+        {
+        	alert("Please select one of the options below");
+        	return;
+        }
+        stepId = stepId + 1;
+        //console.log($('#bullet'+stepId).attr('class'),'classname');
+        $('[id^=diamond]').removeClass('diamond-select');
+        $('#diamond-' + stepId).addClass('diamond-select');
+        for(var i = 0; i < stepId ; i++)
+        {
+        	console.log("for;o")
+        	$('#diamond-' + i).css('color','#0c232c');
+        	// $('#diamond-' + i).addClass('diamond-selected');
+        }
+        $('[class^=label]').css('opacity', '0');
+        $('.label-' + stepId).css('opacity', '1');
+        // $('.label-'+stepId).html(content[stepId]);
+        nextStep = currentStep + 1;
+        console.log(stepId, "stepId");
+        if (nextStep == $steps.length) {
+            alert("You reached the end");
+            return;
+        }
+        $($steps.get(currentStep)).hide();
+        $($steps.get(nextStep)).show();
+        currentStep = nextStep;
+    });
 
 
- $('#prev').on('click', function(e) {
-    e.preventDefault();
-    
-    prevStep = currentStep - 1;
-    if (prevStep < 0) {
-      alert("You reached the end");
-      return;
-    }
-    $($steps.get(currentStep)).hide();
-    $($steps.get(prevStep)).show();
-    currentStep = prevStep;
-  });
-  
-  $(".triangle-up span").html("$2130");
+    $('[id^=prev]').on('click', function(e) {
+        console.log("entered prev click");
+        e.preventDefault();
+        var stepId = parseInt($(this).parent().parent().parent().parent().attr('id').replace(/[a-z,-]/g, ''));
+        for(var i = 1; i <= 15 ; i++)
+        {
+        	if($('#step-' + i ).children().children().children().hasClass('click-selected'))
+        		$('#diamond-' + i).css('color','#0c232c');
+        }
+        stepId = stepId - 1;
+        $('[id^=diamond]').removeClass('diamond-select');
+        $('#diamond-' + stepId).css('color','#fa6023');
+        $('[class^=label]').css('opacity', '0');
+        $('.label-' + stepId).css('opacity', '1');
+        prevStep = currentStep - 1;
+        console.log(prevStep);
+        if (prevStep < 0) {
+            alert("You reached the end");
+            return;
+        }
+        $($steps.get(currentStep)).hide();
+        $($steps.get(prevStep)).show();
+        currentStep = prevStep;
+    });
 
-
-
-var content = ['Index Page','Platform', 'Size', 'User Management','Data Type','Existing Data Sources', 
-'Location Data', 'Third Party Cloud API','User Engagement','Quality', 'Security', 'Analytics'];
-
-text ="";
-for(var i=1; i <= 14; i++)
-{
-  text = text +"<span id=\"bullet"+ i +"\" class=\"bullet\">&#9670;&emsp;&emsp;&emsp;</span>";
-}
-
-text = text+"<span id=\"bullet"+ 15 +"\" class=\"bullet\">&#9670;</span>";
-$('.background').append(text);
-
-var w = $(window).width();
-var h = $(window).height();
-console.log(h,"h");
-var ww=parseInt($('.background').css('width'));
-var marginLeft=w/2-ww/2;
-$('.background').css('margin-left',marginLeft + "px");
-console.log(ww);
-console.log(marginLeft,"marginLeft");
-marginLeft = marginLeft + parseInt(90);
-$('.bulletian-names').css('margin-left',marginLeft + "px");
-// console.log(text,"text");
-
-$('.bullet').click(function(){
-  var myid=parseInt($(this).attr('id').replace(/[a-z]/g, ''))
-  console.log("This is id "+ myid);
-  pageId = "step-"+myid,
-  //console.log($('#'+pageId).attr('value'),'value');
-  console.log("step-"+myid,'stepId');
-});
-/***
-This function is for calculating the total for Multiple select options.
-*/
- $('.multiple-click').on('click', function(event) {
-       dict =  dictionary(jsonData);
+    /***
+    This function is for calculating the total for Multiple select options.
+    */
+    $('.multiple-click').on('click', function(event) {
+        dict = dictionary(jsonData);
         id = $(this).attr('id');
         // console.log(dict[ID]['imageWhite'],"dictionary");
         $(this).toggleClass("click-selected");
@@ -114,65 +113,57 @@ This function is for calculating the total for Multiple select options.
         cost = dict[id]['value'];
         whiteImage = dict[id]['imageWhite'];
         image = dict[id]['image'];
-        if($(self).hasClass("click-selected")){
-          total = total + parseInt(cost);
-          $(self).children(".imagecontainer").children('.imgclass').attr("src", whiteImage);  
-        }
-         else{
-          total = total - parseInt(cost);
+        if ($(self).hasClass("click-selected")) {
+            total = total + parseInt(cost);
+            $(self).children(".imagecontainer").children('.imgclass').attr("src", whiteImage);
+        } else {
+            total = total - parseInt(cost);
             $(self).children(".imagecontainer").children('.imgclass').attr("src", image);;
-         }
-        console.log(total,"total");       
+        }
+        $(".estimated-price").html("$" + total);
     });
 
-/***
-This function is for calculating the total for Single select options.
-*/
- $('.single-click').on('click', function(event) {
+    /***
+    This function is for calculating the total for Single select options.
+    */
+    $('.single-click').on('click', function(event) {
         var mainId = $(this).parent().parent().attr('id');
-        console.log(mainId,"mainId");
-        dict =  dictionary(jsonData);
-        count = $('#'+mainId).find('.single-click').length;
-         for(k = 0 ; k < count ; k++)
-         {
-      id = $('#'+mainId).find('.single-click').eq(k).attr('id');
-      self = $('#'+mainId).find('.single-click').eq(k);
-        cost = dict[id]['value'];
-        console.log(cost,'cost');
-            if($(self).hasClass("click-selected"))
-            {
-              selectedValue = cost;
-              selected = 1;
-            }   
-    }
-        $('#'+mainId).find('.single-click').removeClass("click-selected");
-         // console.log($('#'+mlainId).find('.b'),"$('#'+mainId).find('.b')");
-        $(this).addClass("click-selected"); 
-         // console.log(mainId,"parentid");
-        for(j = 0 ; j < count ; j++)
-         {
-      id = $('#'+mainId).find('.single-click').eq(j).attr('id');
-      self = $('#'+mainId).find('.single-click').eq(j);
-        cost = dict[id]['value'];
-          whiteImage = dict[id]['imageWhite'];
-          image = dict[id]['image'];
-            if($(self).hasClass("click-selected")){
-              total = total + parseInt(cost);
-              $(self).children(".imagecontainer").children('.imgclass').attr("src", whiteImage);  
+        console.log(mainId, "mainId");
+        dict = dictionary(jsonData);
+        count = $('#' + mainId).find('.single-click').length;
+        for (k = 0; k < count; k++) {
+            id = $('#' + mainId).find('.single-click').eq(k).attr('id');
+            self = $('#' + mainId).find('.single-click').eq(k);
+            cost = dict[id]['value'];
+            console.log(cost, 'cost');
+            if ($(self).hasClass("click-selected")) {
+                selectedValue = cost;
+                selected = 1;
             }
-             else{
+        }
+        $('#' + mainId).find('.single-click').removeClass("click-selected");
+        // console.log($('#'+mlainId).find('.b'),"$('#'+mainId).find('.b')");
+        $(this).addClass("click-selected");
+        for (j = 0; j < count; j++) {
+            id = $('#' + mainId).find('.single-click').eq(j).attr('id');
+            self = $('#' + mainId).find('.single-click').eq(j);
+            cost = dict[id]['value'];
+            whiteImage = dict[id]['imageWhite'];
+            image = dict[id]['image'];
+            if ($(self).hasClass("click-selected")) {
+                total = total + parseInt(cost);
+                $(self).children(".imagecontainer").children('.imgclass').attr("src", whiteImage);
+            } else {
 
-             $(self).children(".imagecontainer").children('.imgclass').attr("src", image);;
-             }   
-    }
-             if(selected === 1){
-              total = total-selectedValue;
-             } 
-             selected = 0; 
-              console.log(total,selectedValue,"total,temp");
+                $(self).children(".imagecontainer").children('.imgclass').attr("src", image);;
+            }
+        }
+        if (selected === 1) {
+            total = total - selectedValue;
+        }
+        selected = 0;
+        console.log(total, selectedValue, "total,temp");
+        $(".estimated-price").html("$" + total);
     });
-
-//console.log(total,"total");
 
 });
-
